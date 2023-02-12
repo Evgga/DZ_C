ae8b86319cbc7986b5731dfcdf9a84c1521af8e9
Evgenia <evgga@mail.ru>
2023-02-12 21:30:14 +0900

    У числа показыает вторую цифру слева

---
+++ Test_10/Program.cs
@@ -0,0 +1,8 @@
+﻿// напишите программу, ктр принимает на вход целое число любой разрядности и на выходе показывает вторую цифру 
+//слева этого числа или говорит, что такой цифры нет. Через строку решать нельзя.
+
+
+Console.Write("Введите число: ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+Console.WriteLine("Вторая цифра этого числа -> "+stringNumber[1]);
