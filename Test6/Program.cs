f915524f301c065211dafde53b5d0a591028d04f
Evgenia <evgga@mail.ru>
2023-02-12 20:20:32 +0900

    Число четное или нечетное, делится ли на 2 без остатка

--- Test6/Program.cs
+++ Test6/Program.cs
@@ -1,14 +1,17 @@
-﻿Console.Write("Введите число: ");
+﻿//Напишите программу, ктр на вход принимает число и выдаёт, является ли число чётным(делится ли оно на 2 без остатка)
+Console.Write("Введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
 int remDiv = num % 2;
 
 if(remDiv == 0)
 {
+    Console.WriteLine("Четное");
     Console.WriteLine("Делится на 2 без остатка: Да");
 }
 else
 {
+    Console.WriteLine("Нечетное");
     Console.WriteLine("Делится на 2 без остатка: Нет");
 }
 
